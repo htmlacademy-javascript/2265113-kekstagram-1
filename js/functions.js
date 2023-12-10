3a915f23d206f68c8b29284f143103f6403af5cb
Антон <fareast.vladivostok.chuchalin@gmail.com>
2023-12-10 18:49:47 +1000

    создаст модуль функций

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,10 @@
-const isPalindrome = (text) => {
-  const pureText = text.replaceAll(' ', '').toLowerCase();
-  const middle = pureText.length / 2;
-
-  for (let i = 1; i <= middle; i++) {
-    if (pureText.at(i - 1) !== pureText.at(-i)) {
-      return false;
-    }
-  }
-
-  return true;
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
-isPalindrome('Лёша на полке клопа нашёл ');
 
-const extractingNumber = (value) => {
-  let numberStr = '';
-  const chars = String(value);
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-  for (const i of chars) {
-    if (!Number.isNaN(parseInt(i, 10))) {
-      numberStr += i;
-    }
-  }
-
-  return parseInt(numberStr, 10);
-};
-extractingNumber(2023);
-
-const generateAddress = (sourceString, minLength, addSymbols) =>
-  sourceString.length >= minLength ? sourceString :
-    addSymbols.slice(0, ((minLength - sourceString.length) % addSymbols.length)) + addSymbols.repeat((minLength - sourceString.length) / addSymbols.length) + sourceString;
-generateAddress('1', 2, '0');
-
-const checkLength = (text, maxLength) => text.length <= maxLength;
-checkLength('проверяемая строка', 20);
-
-const getRandomNumber = (min, max, fractionalPart) => (Math.random() * (max - min + 1) + min).toFixed(fractionalPart);
-getRandomNumber(5.015125, 5.212125, 3);
+export {getRandomInteger, getRandomArrayElement};
