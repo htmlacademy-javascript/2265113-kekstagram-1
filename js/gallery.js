86d40576b853ef70e9950a5545a395bea8d6d0e5
Антон <fareast.vladivostok.chuchalin@gmail.com>
2023-12-26 17:45:51 +1000

    добавление описания в alt

--- js/gallery.js
+++ js/gallery.js
@@ -2,9 +2,10 @@ const picturesContainer = document.querySelector('.pictures');
 picturesContainer.querySelector('.pictures__title').classList.remove('visually-hidden');
 const similarPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const createPhotoEl = ({url, likes, comments}) => {
+const createPhotoEl = ({url, description, likes, comments}) => {
   const photoEl = similarPhotoTemplate.cloneNode(true);
   photoEl.querySelector('.picture__img').src = url;
+  photoEl.querySelector('.picture__img').alt = description;
   photoEl.querySelector('.picture__likes').textContent = likes;
   photoEl.querySelector('.picture__comments').textContent = comments.length;
   return photoEl;
