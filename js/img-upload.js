dae26fb86aaeecf149799e29251cbbd874dcbb4d
Антон <fareast.vladivostok.chuchalin@gmail.com>
2024-01-12 00:33:07 +1000

    форма загрузки изображения

---
+++ js/img-upload.js
@@ -0,0 +1,71 @@
+import {isEscapeKey} from './utils.js';
+
+const form = document.querySelector('.img-upload__form');
+const uploadFileField = form.querySelector('.img-upload__input');
+const closeButton = form.querySelector('.img-upload__cancel');
+const overlay = form.querySelector('.img-upload__overlay');
+const body = document.body;
+const hashtagField = form.querySelector('.text__hashtags');
+const commentField = form.querySelector('.text__description');
+const TAG_ERROR_TEXT = 'Не верно указан Хэш-тег';
+const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/;
+const MAX_HASHTAG_COUNT = 5;
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error-title',
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const isElFocused = () => document.activeElement === hashtagField || document.activeElement === commentField;
+
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt.key) && !isElFocused()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+uploadFileField.addEventListener('change', () => {
+  showModal();
+});
+closeButton.addEventListener('click', () => {
+  hideModal();
+});
+
+const isValidTag = (tag) => VALID_SYMBOLS.test(tag);
+
+const isUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const isValidTagsCount = (tags) => tags.length <= MAX_HASHTAG_COUNT;
+
+const validateTags = (value) => {
+  const tags = value.trim().split(' ').filter((tag) => tag.trim().length);
+
+  return tags.every(isValidTag) && isUniqueTags(tags) && isValidTagsCount(tags);
+};
+
+pristine.addValidator(hashtagField, validateTags, TAG_ERROR_TEXT);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {renderGallery} from './gallery.js';
 import {createUsersPhotoList} from './data.js';
+import './img-upload.js';
 
 const photoList = createUsersPhotoList(25);
 
